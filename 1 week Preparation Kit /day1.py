f92ada34d0067a6d5061394ae74a9d57529b859c
gabriel6181997 <gabriel6181997@gmail.com>
2022-04-12 18:05:30 +0900

    ✨ Finish day1 mock test

--- 1 week Preparation Kit /day1.py
+++ 1 week Preparation Kit /day1.py
@@ -181,6 +181,64 @@ if __name__ == '__main__':
 
     fptr.close()
 
+#### Mock Test ####
+### Find the median ###
+
+# The median of a list of numbers is essentially its middle element after sorting. The same number of elements occur after it as before. Given a list of numbers with an odd number of elements, find the median?
+
+# Example
+# arr = [5,3,1,2,4]
+# The sorted array arr' = [1,2,3,4,5]. . The middle element and the median is 3.
+
+# Function Description
+# Complete the findMedian function in the editor below.
+# findMedian has the following parameter(s):
+# ・int arr[n]: an unsorted array of integers
+# Returns
+# ・int: the median of the array
+
+# Input Format
+# The first line contains the integer n, the size of arr.
+# The second line contains n space-separated intergers arr[i].
+
+# Constraints
+# 1<=n<=1000001
+# n is odd
+# -1000 <= arr[i] <= 1000
+
+# Sample Input 0
+# 7
+# 0 1 2 4 6 5 3
+
+# Sample Output 0
+# 3
+
+import math
+import os
+import random
+import re
+import sys
+
+def findMedian(arr):
+    arr.sort()
+    medium = arr[len(arr)//2]
+    return medium
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = findMedian(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+
 
 
 
